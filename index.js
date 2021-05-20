e9fb56c268e7e5163db9e7d22851fce0873e9d67
DelaneyGalvanize <delaney.patterson001@gmail.com>
2021-05-20 10:32:50 -0400

    hook up index.js to button click and get data from api with fetch. need to style and clear input and radio buttons

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Javascript goes here
+function getPhotos() {
+  console.log('in here')
+  let date = document.getElementById('dateValue').value;
+  let btn1IsChecked = document.getElementById('button1').checked;
+  let btn2IsChecked = document.getElementById('button2').checked;
+  let btn3IsChecked = document.getElementById('button3').checked;
+
+  if(btn1IsChecked === true) {
+    roverName = "curiosity";
+  } else if (btn2IsChecked === true) {
+    roverName = "opportunity";
+  } else {
+    roverName = "spirit";
+  }
+
+  const api_key = 'DEMO_KEY'
+  const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?earth_date=${date}&api_key=DEMO_KEY`;
+
+  const fetchRoverPhotos = async () => {
+  try {
+    const response = await fetch(`${url}`)
+    const data = await response.json()
+    console.log(data, 'data')
+    displayData(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+  fetchRoverPhotos()
+  // document.getElementById("dateValue").value = "";
+}
+
+function displayData(data) {
+  let imageContainer = document.getElementById('image-container');
+  imageContainer.innerHTML = "";
+  for (var i = 0; i < data.photos.length; i++) {
+    imageContainer.innerHTML += '<img src=\"'+data.photos[i].img_src+'\">';
+  }
+}
+
+function reset () {
+  // come back and add functionality to clear form after success call
+  console.log('reset form after successful ')
+}
